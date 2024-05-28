11ecfb802b02ce96f0371adeecfae23f766455db
Kuchienkz <wahyupamengas.developer@gmail.com>
2024-05-28 22:16:23 +0800

    set  title

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,8 +7,8 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
+      title: "Scrolla Wallpaper",
+      description: "A Blog for Scrolla Wallpaper guides",
     },
     "/zh/": {
       lang: "zh-CN",
